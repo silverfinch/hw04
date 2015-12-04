51f84f24906eed7e80f36ae463ab1ca9347c7713
Dennis Scheglov <scheglov@astral.phys.uconn.edu>
2015-12-04 13:24:03 -0500

    first commit

---
+++ hw04.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+
+
+struct point
+{
+    double x;
+    double y;
+};
+
+struct triangle
+{
+    struct point a;
+    struct point b;
+    struct point c;
+};
+
+double area (struct triangle t);
+
+double area (struct triangle t)
+{
+    double s;
+
+    s = 0.5 * fabs ((t.b.x - t.a.x) * (t.c.y - t.a.y) - (t.c.x -
+            t.a.x) * (t.b.y - t.a.y));
+    return s;
+}
+
+double distance (struct point a, struct point b);
+
+double distance (struct point a, struct point b)
+{
+    return sqrt (pow ((a.x - b.x), 2.) + pow ((a.y - b.y), 2.));
+}
+
+int main (void)
+{
+    struct triangle t = (struct triangle) { {0, 0.}, {3., 0}, {0., 4.} };
+
+    double a = area (t);
+    double p =
+        distance (t.a, t.b) + distance (t.b, t.c) + distance (t.a, t.c);
+
+    printf ("radius = %f\n", 2 * a / p);
+
+    return 0;
+}
